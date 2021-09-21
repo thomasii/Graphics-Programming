286f86a17a779f64d931ec1c10927b35386c2aff
Thomas Wright <37813874+thomasii@users.noreply.github.com>
2021-09-21 13:04:23 -0400

    Add files via upload

---
+++ clean.bat
@@ -0,0 +1,17 @@
+rmdir Debug /S /Q
+rmdir Release /S /Q
+rmdir x64 /S /Q
+
+rmdir CSE287RasterizationPipeline\Debug /S /Q
+rmdir CSE287RasterizationPipeline\Release /S /Q
+rmdir CSE287RasterizationPipeline\x64 /S /Q
+rmdir Common\Debug /S /Q
+rmdir Common\Release /S /Q
+rmdir Common\x64 /S /Q
+
+rmdir CSE287Raytrace\Debug /S /Q
+rmdir CSE287Raytrace\Release /S /Q
+rmdir CSE287Raytrace\x64 /S /Q
+
+rmdir .vs /S /Q
+
